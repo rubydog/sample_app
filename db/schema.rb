332422d6404777920de81e251c978389d3cafa70
Bhushan Lodha <bhushan@Viksits-MacBook-Pro.local>
2016-04-15 21:58:05 +0530

    Added status to post

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20160405144827) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
+    t.boolean  "active"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
